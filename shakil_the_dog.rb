740a9a4bfd87ec46c337af83a9961d1647ac5f7e
Christopher Leabeter <christopherleabeter@gmail.com>
2015-09-05 13:51:50 -0400

    Initial Commit

---
+++ shakil_the_dog.rb
@@ -0,0 +1,14 @@
+# Save this file to your computer so you can run it 
+# via the command line (Terminal) like so:
+#   $ ruby shakil_the_dog.rb
+#
+# Your method should wait for user input, which corresponds
+# to you saying something to your dog (named Shakil).
+ 
+# You'll probably want to write other methods, but this 
+# encapsulates the core dog logic
+def shakil_the_dog
+end
+ 
+# Run our method
+shakil_the_dog
