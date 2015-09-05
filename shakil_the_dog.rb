f41f9063ca124b521d59cee8c45e03a5d17b542e
Christopher Leabeter <christopherleabeter@gmail.com>
2015-09-05 14:39:04 -0400

    Shakil The Dog - program runs as intended

--- shakil_the_dog.rb
+++ shakil_the_dog.rb
@@ -8,6 +8,21 @@
 # You'll probably want to write other methods, but this 
 # encapsulates the core dog logic
 def shakil_the_dog
+  loop do
+    # puts "Say something to Shakil"
+    command = gets.chomp
+    if command == "woof"
+      puts "Shakil: 'WOOF WOOF WOOF!'"
+    elsif command == "shakil stop" || command == "Shakil STOP!"  
+    elsif command == "meow"
+      puts "Shakil: 'woof woof woof woof woof'"
+    elsif command.include? "treat"
+    elsif command == "go away"
+      abort
+    else
+      puts "Shakil: 'Woof!'"
+    end
+  end
 end
  
 # Run our method
